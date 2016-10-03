13a992cf8d9a824515609e6b85397dbf9a319447
mkspramuditha <mkspramuditha@gmail.com>
2016-10-03 20:21:04 +0530

    pushing online

--- dbConnect.php
+++ dbConnect.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shan
- * Date: 10/3/2016
- * Time: 7:01 PM
- */
+$servername = "localhost";
+$username = "root";
+$password = null;
+$dbName = "robogames";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+//$time = new \DateTime();
+//$time = $time->format('H:m:s');
+////$sql = "INSERT INTO team_time (time_record)
+////VALUES ('$time')";
+//
+//if ($conn->query("INSERT INTO `team_time` (`time_record`) VALUES ('$time')") === TRUE) {
+//    echo "Table MyGuests created successfully";
+//} else {
+//    echo "Error creating table: " . $conn->error;
+//}
+//
+//$conn->close();
+?>
--- insertTime.php
+++ insertTime.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shan
- * Date: 10/3/2016
- * Time: 7:01 PM
- */
+include("dbConnect.php");
+
+$time = $_POST['time'];
+
+
+if ($conn->query("INSERT INTO `team_time` (`record_time`) VALUES ('$time')") === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();

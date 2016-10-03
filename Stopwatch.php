16a17a6a70256c0232c41c4d97ea7352bc75516a
nipunasudha <nipuna.14@cse.mrt.ac.lk>
2016-10-03 15:33:05 +0530

    tabs added
    styled the basics, timer plugin added

--- Stopwatch.php
+++ Stopwatch.php
@@ -33,8 +33,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Stopwatch</a></li>
+        <li ><a href="#">Home</a></li>
+        <li class="active"><a href="#">Stopwatch</a></li>
         <li><a href="#">Score Board</a></li>
         
       </ul>

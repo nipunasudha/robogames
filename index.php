1b45a5e737664c146acb8401796445bacd941288
nipunasudha <nipuna.14@cse.mrt.ac.lk>
2016-10-03 15:07:25 +0530

    Init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+     <h1>IESL RoboGames 2016</h1>
+   
+ <?php
+$dir = "./teams";
+// Open a directory, and read its contents
+foreach(glob($dir.'/*.*') as $file) {
+	//echo "<img src='".$file."'>";
+}
+?>
+    </body>
+</html>
